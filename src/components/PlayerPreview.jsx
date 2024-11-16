538b72657842bf02f591633db1f8f9f4f501428b
israel litvak <lisrael1989@gmail.com>
2024-11-16 17:15:42 +0200

    Add the option to vote per player

--- src/components/PlayerPreview.jsx
+++ src/components/PlayerPreview.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 export function PlayerPreview({ player }) {
+  const [vote, setVotes] = useState(0);
+
+  function handleVote() {
+    setVotes(vote + 1);
+  }
+
   return (
-    <article className="player-preview">
-      <h3 className="player-name">{player.name}</h3>
-      <img className="player-img" src={player.img} alt={player.name} />
-    </article>
+    <>
+      <article className="player-preview">
+        <p className="vote-txt">
+          Votes: <span>{vote}</span>
+        </p>
+        <h3 className="player-name">{player.name}</h3>
+        <img className="player-img" src={player.img} alt={player.name} />
+      </article>
+      <button className="vote-btn" onClick={handleVote}>
+        Vote
+      </button>
+    </>
   );
 }
