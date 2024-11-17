c53147d2daa72aaee502d963d91f39c110e34ae9
israel litvak <lisrael1989@gmail.com>
2024-11-17 13:54:19 +0200

    Made changes in the home page jsx file

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -8,14 +8,14 @@ export function HomePage() {
           <span>Welcome</span> to <span> VOTE</span> App!
         </h1>
         <h2 className="sub-title-home">
-          Let start the to Vote for your favorite Player
+          Let start the to Vote for your favorite <span> NBA</span> Player
         </h2>
       </div>
       <div>
         <Link to="/players">
           <img
             className="home-img animate__animated animate__bounce"
-            src="/public/images/vote3.png"
+            src="/public/hero.png"
           ></img>
         </Link>
       </div>
